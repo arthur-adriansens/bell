0dd7f24f5adcdd9c66bdf51c76adbb28c97ed681
arthur_adriansens <arthur.adriansens@gmail.com>
2023-12-24 11:46:28 +0100

    v2, added server code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+<!-- @format -->
+
 # bell
-A bell that rings when a new client is made, powered by Microsoft Forms and the Raspberry pi 4.
+
+## discription
+
+A bell that rings when a new client is created, powered by Microsoft Forms and the Raspberry pi 4.
+
+## install
+
+`git clone https://github.com/arthur-adriansens/bell && cd bell`
+`npm install`
+`pm2 startup && pm2 save`
+
+## testing
+
+`npm run test` (equal to `pm2 monit`)
---
+++ server/index.js
@@ -0,0 +1,66 @@
+/** @format */
+
+const fs = require("fs");
+const player = require("play-sound");
+const inbox = require("inbox");
+const cron = require("node-cron");
+require("dotenv").config({ path: "../.env" });
+
+// cron.schedule("*/5 * * * *", () => {
+cron.schedule("*/1 * * * *", () => {
+    const client = inbox.createConnection(false, "outlook.office365.com", {
+        secureConnection: true,
+        auth: {
+            user: process.env.email,
+            pass: process.env.password,
+        },
+    });
+
+    client.connect();
+
+    client.on("connect", () => {
+        console.log("Successfully connected to server:");
+
+        client.openMailbox("INBOX", (error, info) => {
+            if (error) throw error;
+            console.log("   message count in INBOX: " + info.count);
+
+            client.listMessages(0, (err, messages) => {
+                messages.forEach((message) => {
+                    console.log(message.UID + ": " + message.title);
+                    handleMessage(message);
+                });
+            });
+        });
+
+        client.close();
+    });
+
+    client.on("close", () => {
+        console.log("Successfully disconnected!");
+    });
+});
+
+function handleMessage(message) {
+    if (message.title == "bell123") {
+        console.log("ringgg");
+
+        client.deleteMessage(message.UID, (err) => {
+            if (err) throw err;
+            console.log(`deleted "${message.title}"`);
+        });
+    } else {
+        client.deleteMessage(message.UID, (err) => {
+            if (err) throw err;
+            console.log(`deleted ${message.title}`);
+        });
+    }
+}
+
+function playSound() {
+    const sounds = fs.readdirSync("../sounds");
+    console.log(sounds);
+    player.play("path/to/audio/file.mp3", function (err) {
+        if (err) throw err;
+    });
+}
