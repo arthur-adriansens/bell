b8ae61b01ae4de26cb271b751d205a735d293e1f
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-03-14 21:32:30 +0100

    test

--- server/index.js
+++ server/index.js
@@ -113,15 +113,12 @@ cron.schedule("*/1 * * * *", () => {
     function update() {
         console.log("updating...");
 
-        exec(
-            "git stash push -- .env && git stash push -- sounds/ && git pull --rebase origin main && git stash pop stash@{0} && git stash pop stash@{1} && npm install && sudo reboot",
-            (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
+        exec("git stash && git pull --rebase origin main && git stash pop && npm install && sudo reboot", (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
             }
-        );
+            console.log(`stdout: ${stdout}`);
+        });
     }
 });
